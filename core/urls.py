cf9229e13d3831aa4968117ec3f4ff27632f7b9c
Adithya Dev <adithyadev00000@gmail.com>
2026-01-03 14:03:43 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = []
-
-# ✅ STATIC FIRST (CRITICAL)
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
-
-# ✅ THEN normal routes
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('admin_black.urls')),
     path('', include('home.urls')),
 ]
+
+# ✅ CORRECT way to serve static files in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 
 
