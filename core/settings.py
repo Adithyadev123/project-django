099f33886f07a9d4311c262220adacfd2e4f1066
Adithya Dev <adithyadev00000@gmail.com>
2026-01-05 11:32:15 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "wsertyuio-1656982469.ap-south-1.elb.amazonaws.com"
+    ""http://wsertyuio-1656982469.ap-south-1.elb.amazonaws.com""
 ]
 
 # --------------------------------------------------
